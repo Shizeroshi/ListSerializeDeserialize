8197ec3f92928823fe38bb0bb0cb7a3ba52d9e0b
WeCryAlone <75624361+WeCryAlone@users.noreply.github.com>
2021-08-31 14:36:37 +0200

    Add files via upload

---
+++ PC.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ClassLib
+{
+    public class PC
+    {
+        public delegate string Computer(string message);
+        Computer console;
+
+        public string Mark { get; set; }
+        public int SerialNumber { get; set; }
+
+        public bool IsOn = false;
+        //public string On = "Computer is turned on";
+        //public string Off = "Computer is turned off";
+        //public string Reb = "Rebooting...";
+
+
+        public PC()
+        {
+            Mark = "LoL";
+            SerialNumber = 1488;
+        }
+
+        public PC(string mark, int serialNumber)
+        {
+            Mark = mark;
+            SerialNumber = serialNumber;
+        }
+
+        public void TurnOn()
+        {
+            IsOn = true;
+            console(">Computer is turned on");
+        }
+        public void TurnOff()
+        {
+            IsOn = false;
+            console(">Computer is turned off");
+        }
+        public void Reboot()
+        {
+            IsOn = false;
+            console(">Computer is turned off");
+            Thread.Sleep(200);
+            console(">Rebooting...");
+            Thread.Sleep(200);
+            IsOn = true;
+            console(">Computer is turned on");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.Web.Script.Serialization;
+using ClassLib;
+using System.Diagnostics;
+
+namespace SerializConsolApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var computer = new List<PC>()
+            {
+                new PC() { Mark = "Lenovo", SerialNumber = 1337 },
+                new PC() { Mark = "Intel", SerialNumber = 228 },
+                new PC() { Mark = "Dell", SerialNumber = 322 },
+                new PC() { Mark = "HP", SerialNumber = 5051 }
+            };
+
+
+            string Dir = @"D:\TestFolder\listSerial.txt";
+            var Serializer = new JavaScriptSerializer();
+            File.WriteAllText(Dir, (Serializer).Serialize(computer));
+            Serializer.Deserialize<List<PC>>(File.ReadAllText(Dir));
+            var OpenFile = Process.Start(Dir);
+            
+
+            
+
+        }
+    }
+}
